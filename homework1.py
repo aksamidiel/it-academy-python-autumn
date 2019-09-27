d101c7faf05f5bfec1c71d27c5e8c22a1521f839
aksamidiel <38276155+aksamidiel@users.noreply.github.com>
2019-09-27 13:47:46 +0300

    homework

--- homework1.py
+++ homework1.py
@@ -22,13 +22,12 @@ greet('Jack')
 greet('Jill')
 greet('Bob')
 
-
-#test_palindrome
-number = input('Enter number: ')
-n = number[::-1] #переворот чтение введенного с последнего символа <----
-if number == n:
-  print("yes, this is palindrome")
-else:
-  print("no, this is not palindrome")
+#task6
+import re
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print (test_string, 'is a valid US local phone number')
+    else:
+        print (test_string, 'rejected')
 
 
