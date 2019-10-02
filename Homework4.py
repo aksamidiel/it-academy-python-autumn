989609b3050394fc2eef70c7826415da2bfdb216
aksamidiel <38276155+aksamidiel@users.noreply.github.com>
2019-10-02 22:29:19 +0300

    final fix

--- Homework4.py
+++ Homework4.py
@@ -43,7 +43,8 @@ print("Result: ", lst_1)
 print("create list with list comprehension ")
 # n = int(input("Enter number of range: "))
 
-lst_1 = [[a + b] for a in 'abcd' if 'a' <= a <= 'd' for b in 'abcd' if 'b' <= b <= 'd']
+lst_1 = [[a + b] for a in 'abcd' if 'a' <= a <= 'd'
+         for b in 'abcd' if 'b' <= b <= 'd']
 print("Result: ", lst_1)
 
 flat_list = [item for sublist in lst_1 for item in sublist][:6]
