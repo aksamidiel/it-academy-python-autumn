da3c17b3b190c8ed8fc3668f964883582460a416
aksamidiel <38276155+aksamidiel@users.noreply.github.com>
2019-10-02 22:23:45 +0300

    changes

--- Homework4.py
+++ Homework4.py
@@ -1,5 +1,4 @@
-
-# 1. Write a program that prints the numbers from 1 to 100,
+# Write a program that prints the numbers from 1 to 100
 # but for multiples of three print “Fizz” instead of the
 # number and for multiples of five print “Buzz”.
 # For numbers which are multiples of both three and five,
