7920eb7cb5931a443aa372b1d466a8fbcc83a033
aksamidiel <38276155+aksamidiel@users.noreply.github.com>
2019-10-07 23:24:59 +0300

    addon by 2 task

--- test1.py
+++
@@ -1,6 +0,0 @@
-some_simple_string = input("Enter some string: ").replace(' ', '_')
-while "__" in some_simple_string:
-    some_simple_string = some_simple_string.replace('__', '_')
-print(some_simple_string)
-
-
