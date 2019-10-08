39e23d111c0310d188e9e19b18e41db4f7b5a756
aksamidiel <38276155+aksamidiel@users.noreply.github.com>
2019-10-08 16:52:47 +0300

    changes in task's

--- homework2.py
+++ homework2.py
@@ -1,23 +1,33 @@
 # homework2
-
+#
+# Our cost of item's v.2
 rub = int(input('Enter cost in rub(please use whole number: '))
 coins = int(input('Enter cost in coins(please use whole number <100 : '))
 number_of_items = int(input("Input number of item: "))
 
-rub_our = rub * number_of_items
-coin_our = coins * number_of_items
-if coin_our >= 100:
-    rub_our += coin_our // 100
-    coin_our = coin_our % 100
-print('Our cost: rub - {} coins - {} '.format(rub_our, coin_our))
-
-# task_palindrome
-number = input('Enter number: ')
-n = number[::-1]  # переворот чтение введенного с последнего символа <----
-if number == n:
-    print("yes, this is palindrome")
+if coins * number_of_items >= 100:
+    rub = rub * number_of_items + (coins * number_of_items) // 100   # число рублей с учетом монет
+    coins = (coins * number_of_items) % 100          # остаток монеток
 else:
-    print("no, this is not palindrome")
+    rub = rub * number_of_items
+    coins = coins * number_of_items
+
+print('Our cost: rub - {} coins - {} '.format(rub, coins))
+
+# task_palindrome v.2
+some_number = int(input("Enter some number: "))
+temp = some_number
+reverse = 0
+
+while some_number > 0:
+    digit = some_number % 10
+    reverse = reverse * 10 + digit
+    some_number = some_number // 10
+
+if temp == reverse:
+    print("This number is palindrome")
+else:
+    print("This number is not palindrome")
 
 # task_fibonacci
 # fibonacci start to 1
