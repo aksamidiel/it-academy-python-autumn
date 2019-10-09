6fe54f8f94f6427ac2a48db828d90e442d630e13
aksamidiel <38276155+aksamidiel@users.noreply.github.com>
2019-10-09 13:48:23 +0300

    test

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
+res = []
+some_text = input("Enter some text: ").split()
+print(some_text)
+for st in some_text:
+    print(st)
+    res += st.rstrip('/n')
+print(res)
 
+#print(some_text)
 
+dict_res = dict((word, res.count(word)) for word in set(res))
+
+print(dict_res)
