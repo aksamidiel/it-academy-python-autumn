cdf3f95309051f3d3234921cb56f980d53aeff51
aksamidiel <38276155+aksamidiel@users.noreply.github.com>
2019-10-14 13:10:12 +0300

    some changes

--- homework3.py
+++ homework3.py
@@ -1,49 +1,29 @@
-# Найти самое длинное слово в введенном предложении.
-# Учтите что в предложении есть знаки препинания
+"""Найти самое длинное слово в введенном предложении.
+Учтите что в предложении есть знаки препинания"""
 phrase = input("Please enter some phrase with signs separated by space: ")
 
 print("Entered phrase: ", phrase)
 
-# out = "".join(k for k in phrase if k not in('!', '.', ','))
-# print(out)
+signs = '.,/:;&^%$#@!><+=_-*'
+for j in signs:   # поиск и замена символов
+    phrase = phrase.replace(j, '')
 
-signs = ['.', ',', ':', ';', '!', '?', '(', ')',
-         ' .', ' ,', ' :', ' ;', ' !', ' ?', ' (', ' )',
-         '. ', ', ', ': ', '; ', '! ', '? ', '( ', ') ']
 list_word = phrase.split()
-i = 0
-
-for word in list_word:
-    if word[-1] in signs:
-        list_word[i] = word[:-1]
-        word = list_word[i]
-    if word[0] in signs:
-        list_word[i] = word[1:]
-    i += 1
-
-i = 0
-while i < len(list_word):
-
-    print(list_word[i], end=' \n')
-    i += 1
-
-long_word = 0
+length_some_word = len(list_word[-1])
+word = ''
 for i in range(1, len(list_word)):
-    if len(list_word[long_word]) < len(list_word[i]):
-        long_word = i
+    if length_some_word < len(list_word[i]):
+        length_some_word = len(list_word[i])
+        word = list_word[i]
+print("This is longest word in phrase: ", word)
 
-print('long word/words: ', list_word[long_word])
-print("length of word: ", len(list_word[long_word]))
-
-# task2: Вводится строка. Требуется удалить из нее
-# повторяющиеся символы и все пробелы.
-# Например, если было введено "abc cde def",
-# то должно быть выведено "abcdef".
+""" task2: Вводится строка. Требуется удалить из нее
+ повторяющиеся символы и все пробелы.
+ Например, если было введено "abc cde def",
+ то должно быть выведено "abcdef"."""
 
 phrase = input("Please enter simple phrase: ")
 print("Entered phrase: ", phrase)
-
-# list_word = phrase.split()
 
 newPhrase = ''
 for i in phrase:
@@ -51,8 +31,8 @@ for i in phrase:
         newPhrase += i
 print(newPhrase)
 
-# Посчитать количество строчных (маленьких) и прописных
-# (больших) букв в введенной строке. Учитывать только английские буквы.
+""" Посчитать количество строчных (маленьких) и прописных
+(больших) букв в введенной строке. Учитывать только английские буквы. """
 
 phrase = input("Please enter simple phrase: ")
 print("Entered phrase: ", phrase)
