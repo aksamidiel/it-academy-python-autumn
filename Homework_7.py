c8e55106bf5ac2997385e77cc88a865624f267a3
aksamidiel <38276155+aksamidiel@users.noreply.github.com>
2019-10-27 23:40:03 +0300

    fix some part in class

--- Homework_7.py
+++ Homework_7.py
@@ -4,15 +4,7 @@
 # Objects should contain attributes and methods to simulate some use cases.
 # Completed program should print object states, it actions (methods) and objects interaction.
 
-def create_Person():
-    name = str(input("Enter name of person: "))
-    age = int(input("Enter age of person: "))
-    sex = str(input("Enter sex of person: "))
-
-    return name, age, sex
-
-
-class Person(create_Person()):
+class Person:
     def __init__(self, name, age, sex):
         self.name = name
         self.age = age
@@ -23,25 +15,35 @@ class Person(create_Person()):
         stat = str(input("Enter status: "))
         num = int("Input number of room: ")
         num_of_night = int(input("Enter night in hotel: "))
-        income = int(input("Enter money income of person($): "))
-        visit_purpose = str(input("Enter visit purpose: "))
 
-        return stat, num, num_of_night, income, visit_purpose
+        print("Status: {}, Number_of_room: {}, num_of_night: {}", stat, num, num_of_night)
 
     @staticmethod
     def special_offers():
         level_of_room = str(input("Level of comfort in room: "))
-        spec_discount = int(input("Enter discount: "))
+        print(level_of_room)
 
-        return level_of_room, spec_discount
-
-    print("Parameters of {} ".format())
-
-
-
+    def display_info(self):
+        print("Some info about person: ", self.name, self.age, self.sex)
 
 
 class Buisnesman(Person):
-    def __init__(self, ):
+    def __init__(self, name, age, sex, company):
+        Person.__init__(self, name, age, sex)
+        self.company = company
 
+    def display_info(self):
+        Person.display_info(self)
+        print("Company: ", self.company)
 
+    def special_offers(self):
+        Person.special_offers(self)
+        spec_discount = int(input("Enter discount: "))
+        meeting_at_arrival = str(input("Place of met: "))
+        print("Discount: {}, Meet: {}", spec_discount, meeting_at_arrival)
+
+    def changes_Person(self):
+        Person.changes_Person(self)
+        visit_purpose = str(input("Enter visit purpose: "))
+        income = int(input("Enter money income of person($): "))
+        print("Visit_purpose: {}, Income: {}", visit_purpose, income)
